11e3b58789d48729a935d8886718ad2bddbc6208
Jeremy Mogro <mogrojeremy@gmail.com>
2024-10-17 22:41:26 -0500

    frist

---
+++ copy/js/firebase.js
@@ -0,0 +1,24 @@
+/*
+  CCTB Website Development
+  IST105
+  Oct 2024
+  Description: This is a simple login website where students are asked to 
+  implement Social Network Login with Firebase
+*/
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDT6E3j4NqKktZmyFJm2yJyzcTB0jNk1VQ",
+  authDomain: "ist105midtermexamjeremymogro.firebaseapp.com",
+  projectId: "ist105midtermexamjeremymogro",
+  storageBucket: "ist105midtermexamjeremymogro.appspot.com",
+  messagingSenderId: "409927718249",
+  appId: "1:409927718249:web:f72483faa1ed38166e99f8"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
